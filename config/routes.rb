24f176ec7c691485f25295362a515f27c3cbc7d7
manoel bertin <manoeltirj@gmail.com>
2023-08-03 09:04:55 -0300

    Está listando os hash apenas no browser, no POSTMAN ainda não está funcionando, falta deslogar e entrar com o usuário freddie mercury.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,33 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  get 'home/index'
+  root 'home#index'
 
-  namespace :api, defaults: { format: :json } do 
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite',  { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end 
+
+
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
+      resources :categories, only: [:index, :show]
+      resources :search, only: :index
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+      end
+      resources :favorites, only: :index
+
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
     end
   end
- end
+  get "*path", to: "home#index", :constraints => lambda{|req| req.path !~ /\.(png|jpg|js|css|json|mp3)$/ }
+end
+
  
